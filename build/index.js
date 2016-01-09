9b58be8dd3c18799737ee2e31171957972cdb7f4
Tan Nguyen <tan@sharkpunch.com>
2016-01-09 11:12:55 +0200

    Build

--- build/index.js
+++ build/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -28,7 +28,7 @@ function detechGlobalNamespace() {
 var NODE = Symbol('node');
 var BROWSER = Symbol('browser');
 
-var Util = (function (_EventEmitter) {
+var Util = function (_EventEmitter) {
   _inherits(Util, _EventEmitter);
 
   function Util() {
@@ -137,6 +137,6 @@ var Util = (function (_EventEmitter) {
   }]);
 
   return Util;
-})(EventEmitter);
+}(EventEmitter);
 
 module.exports = new Util();
